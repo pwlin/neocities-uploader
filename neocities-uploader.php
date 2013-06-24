9ebb3d940ac2820ced68d901861688f4a3e2b1e0
pwlin <pwlin05@gmail.com>
2013-06-24 18:26:15 +0200

    Added csrf_token when uploading files

--- neocities-uploader.php
+++ neocities-uploader.php
@@ -32,7 +32,6 @@ class NeoCities_Uploader {
             CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_CONNECTTIMEOUT => 30,
             CURLOPT_TIMEOUT => 35,
-            CURLOPT_POST => 1,
             CURLOPT_HTTPHEADER => array(
                     'Connection: close',
                     'User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:20.0) Gecko/20100101 Firefox/20.0'
@@ -47,13 +46,12 @@ class NeoCities_Uploader {
         $cookie = tempnam(sys_get_temp_dir(), 'neo');
         $this->curl_default_options[CURLOPT_COOKIEJAR] = $cookie;
         $this->curl_default_options[CURLOPT_COOKIEFILE] = $cookie;
-    }
+   }
 
     public function init() {
         $this->recursive_dir();
         $this->login();
         foreach($this->files_list as $file) {
-            echo("\nUploading $file\n");
             $this->upload($file);    
         }
         echo("\nFinished Uploading\n");
@@ -68,28 +66,58 @@ class NeoCities_Uploader {
     }
     
     private function login() {
+        echo("\nLogging in ...");
         $curl = curl_init();
         $options = $this->curl_default_options;
         $options[CURLOPT_URL] = $this->root_url . '/signin';
+        $options[CURLOPT_POST] = 1;
         $options[CURLOPT_POSTFIELDS] = http_build_query(array(
                 'username' => $this->username,
-                'password' => $this->password
+                'password' => $this->password,
+                'csrf_token' => $this->get_csrf_token($this->root_url . '/signin')
         ));
         curl_setopt_array($curl, $options);
         $content = curl_exec($curl);
         curl_close($curl);
+        echo("Done.\n");
     }
     
     private function upload($file) {
+        echo("\nUploading $file ...");
         $curl = curl_init();
         $options = $this->curl_default_options;
         $options[CURLOPT_URL] = $this->root_url . '/site_files/upload';
+        $options[CURLOPT_POST] = 1;
         $options[CURLOPT_POSTFIELDS] = array(
-                'newfile' => '@' . $file
+                'newfile' => '@' . $file,
+                'csrf_token' => $this->get_csrf_token($this->root_url . '/site_files/new')
         );
         curl_setopt_array($curl, $options);
         $content = curl_exec($curl);
         curl_close($curl);
+        echo("Done.\n");
+    }
+    
+    private function get_csrf_token($url) {
+        $curl = curl_init();
+        $options = $this->curl_default_options;
+        $options[CURLOPT_URL] = $url;
+        curl_setopt_array($curl, $options);
+        $content = curl_exec($curl);
+        libxml_use_internal_errors(true);
+        $dom = new DomDocument();
+        $dom->loadHTML($content);
+        $inputs = $dom->getElementsByTagName('input');
+        $csrf_token = '';
+        foreach($inputs as $input) {
+            if ($input->getAttribute('name') == 'csrf_token') {
+                $csrf_token = $input->getAttribute('value');
+                break;
+            }
+        }
+        unset($dom);          
+        curl_close($curl);
+        return $csrf_token;
     }
     
     
